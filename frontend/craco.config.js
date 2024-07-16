74118c8605166341f0c58c3052134af8e491c625
Shaneel Reddy <shaneelreddy320@gmail.com>
2024-07-16 21:11:03 +0530

    packages added

---
+++ frontend/craco.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    webpack: {
+      resolve: {
+        fallback: {
+          "fs": false,
+          "path": require.resolve("path-browserify"),
+          "os": require.resolve("os-browserify/browser")
+        }
+      }
+    }
+  };
+  
